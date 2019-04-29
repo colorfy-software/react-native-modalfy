ade280be7de0de91cac50ba68632598fa2183d5e
Khaled Osman <khaled.osmaan@gmail.com>
2019-04-29 14:52:07 +0200

    refactor: use rest parameters and fix using ++ on constant variable

--- utils/invariant.js
+++ utils/invariant.js
@@ -8,7 +8,7 @@ if (process.env !== 'production') {
   }
 }
 
-export default function(condition, format, a, b, c, d, e, f) {
+export default function(condition, format, ...args) {
   validateFormat(format)
 
   if (!condition) {
@@ -18,11 +18,10 @@ export default function(condition, format, a, b, c, d, e, f) {
         'Minified exception occurred; use the non-minified dev environment for the full error message and additional helpful warnings.'
       )
     } else {
-      const args = [a, b, c, d, e, f]
       const argIndex = 0
       error = new Error(
         format.replace(/%s/g, function() {
-          return args[argIndex++]
+          return args[argIndex + 1]
         })
       )
       error.name = 'Invariant Violation'
