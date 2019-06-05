63c2c832b9d4f44b51274c6477a955a4c36aad0f
Charles Mangwa <charlesmangwa@gmail.com>
2019-06-05 20:11:28 +0200

    Fix validateListener types 🛠️

--- utils/validateListener.js
+++ utils/validateListener.js
@@ -1,16 +1,16 @@
 /* @flow */
 
-import type { EventName } from '../types'
+import type { EventCallback, EventName, EventNames } from '../types'
 import invariant from './invariant'
 
 type Action = 'add'
 
 type Payload = {
   eventName: EventName,
-  handler: () => void,
+  handler: EventCallback,
 }
 
-const validEventNames = ['onAnimate']
+const validEventNames: EventNames = ['onAnimate']
 
 export default function(action: Action, payload: Payload) {
   const { eventName, handler } = payload
