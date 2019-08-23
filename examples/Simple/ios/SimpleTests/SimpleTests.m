25dac8aad4f63e41d359ee5ac20d96ead0ddc3f7
Charles Mangwa <charlesmangwa@gmail.com>
2019-08-23 16:20:52 +0200

    Update Simple example to React Native 0.60.5 ☝

--- examples/Simple/ios/SimpleTests/SimpleTests.m
+++ examples/Simple/ios/SimpleTests/SimpleTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface examplesTests : XCTestCase
+@interface SimpleTests : XCTestCase
 
 @end
 
-@implementation examplesTests
+@implementation SimpleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
