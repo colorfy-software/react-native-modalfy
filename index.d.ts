0dc163be893a33f0d4131374f4789b7c00118efd
Charles Mangwa <charlesmangwa@gmail.com>
2020-01-09 16:00:52 +0100

    Improve TypeScript interfaces 🔧

--- index.d.ts
+++ index.d.ts
@@ -9,14 +9,16 @@ type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
 
 // From react-native/Libraries/Animated/src/nodes/AnimatedInterpolation
 type ExtrapolateType = 'extend' | 'identity' | 'clamp'
+
 interface InterpolationConfigType {
   inputRange: Array<number>
-  outputRange: Array<number> | Array<string>
+  outputRange: Array<number> | Array<string>-
   easing?: (input: number) => number
   extrapolate?: ExtrapolateType
   extrapolateLeft?: ExtrapolateType
   extrapolateRight?: ExtrapolateType
 }
+
 interface AnimatedInterpolation {
   interpolate: (config: InterpolationConfigType) => AnimatedInterpolation
 }
@@ -25,14 +27,16 @@ interface AnimatedInterpolation {
 interface EndResult {
   finished: boolean
 }
-type EndCallback = (result: EndResult) => void
+
+type EndCallback = (result: EndResult) => void | void
+
 interface Animation {
   start: (
     fromValue: number,
     onUpdate: (value: number) => void,
-    onEnd: EndCallback?,
-    previousAnimation: Animation?,
-    animatedValue: AnimatedValue,
+    onEnd: EndCallback,
+    previousAnimation: Animation | void,
+    animatedValue: AnimatedValue
   ) => void
   stop: () => void
 }
@@ -44,7 +48,7 @@ interface AnimatedTracking {
     parent: any,
     animationClass: any,
     animationConfig: Object,
-    callback?: EndCallback?,
+    callback?: EndCallback
   ) => void
   update: () => void
 }
@@ -63,7 +67,7 @@ interface AnimatedValue {
   stopAnimation: (callback?: (value: number) => void | undefined) => void
   resetAnimation: (callback?: (value: number) => void | undefined) => void
   interpolate: (config: InterpolationConfigType) => AnimatedInterpolation
-  animate: (animation: Animation, callback: EndCallback?) => void
+  animate: (animation: Animation, callback: EndCallback) => void
   stopTracking: () => void
   track: (tracking: AnimatedTracking) => void
 }
@@ -72,42 +76,98 @@ interface AnimatedValue {
 interface AnimationConfig {
   isInteraction?: boolean
   useNativeDriver?: boolean
-  onComplete?: ?EndCallback
+  onComplete?: EndCallback
   iterations?: number
 }
 
 // From react-native/Libraries/Animated/src/animations/TimingAnimation.js
 type TimingAnimationConfig = AnimationConfig & {
-  toValue?: number & AnimatedValue & { x: number, y: number }
+  toValue?: number & AnimatedValue & { x: number; y: number }
   easing?: (value: number) => number
   duration?: number
   delay?: number
+}
+
+/**
+ * INTERNAL TYPES
+ */
+
+interface ModalStackItem {
+  name: ModalName
+  component: React.Component & { modalOptions?: ModalStackOptions }
+  hash: string
+  index: number
+  options?: ModalStackOptions
+  params?: any
+}
+
+interface ModalStack<T> {
+  names: Array<ModalName>
+  content: Array<ModalStackItem>
+  defaultOptions: ModalStackOptions
+  openedItems: T
+  total: number
+}
+
+interface EventListeners {
+  event: string
+  handler: ModalEventCallback
+}
+
+type ModalEventListeners = Set<EventListeners>
+
+interface SharedProps {
+  clearListeners: (hash: string) => void
+  closeAllModals: () => void
+  closeModal: (stackItem: ModalStackItem) => void
+  closeModals: (modalName: ModalName) => void
+  currentModal: ModalName | null
+  eventListeners: ModalEventListeners
+  getParams: (hash: string | void, fallback: any) => any
+  openModal: (modalName: ModalName) => void
+  registerListener: (
+    hash: string,
+    eventName: ModalEventName,
+    handler: () => void
+  ) => ModalEventSubscription
+  stack: ModalStack<Set<ModalStackItem>>
 }
 
 /********
  * TYPES
  *******/
 
-export type ModalName = string
-
-export type TransitionOptions = (animatedValue: AnimatedValue) => {
-  [key: string]:
-    | AnimatedInterpolation
-    | Array<{ [key: string]: AnimatedInterpolation }>
-}
-
-export interface Config {
-  [key: ModalName]: React.ComponentType<any>
+/**
+ * Interface to be used to type `createModalStack()`'s
+ * 1st argument, library's default config.
+ */
+export interface ModalStackConfig {
+  [key: string]: React.ComponentType<any>
   animateInConfig?: TimingAnimationConfig
   animateOutConfig?: TimingAnimationConfig
   containerStyle?: ViewStyle
   modal?: React.ComponentType<any>
   position?: 'center' | 'top' | 'bottom'
   shouldAnimateOut?: boolean
-  transitionOptions?: TransitionOptions
+  transitionOptions?: ModalTransitionOptions
 }
 
-export interface Options {
+export type ModalName = string
+export type ModalTransitionOptions = (
+  animatedValue: AnimatedValue
+) => {
+  [key: string]:
+    | AnimatedInterpolation
+    | Array<{ [key: string]: AnimatedInterpolation }>
+}
+
+/**
+ * Interface to be used to type `createModalStack()`'s
+ * 2nd argument, library's default custom options.
+ * Can also be used to type `static modalOptions: ModalStackOptions`
+ * inside a modal component ().
+ */
+export interface ModalStackOptions {
   animateInConfig?: TimingAnimationConfig
   animateOutConfig?: TimingAnimationConfig
   backdropOpacity?: number
@@ -115,50 +175,28 @@ export interface Options {
   containerStyle?: ViewStyle
   position?: 'center' | 'top' | 'bottom'
   shouldAnimateOut?: boolean
-  transitionOptions?: TransitionOptions
+  transitionOptions?: ModalTransitionOptions
 }
 
-export interface StackItem {
-  name: ModalName
-  component: React.Component & { modalOptions?: Options }
-  hash: string
-  index: number
-  options?: Options
-  params?: any
+/**
+ * Interface to be used inside a component to type the
+ * `modal` prop provided by `withModal` / `useModal`.
+ */
+export interface ModalProp {
+  currentModal: ModalName | null
+  openModal: (modalName: ModalName, params?: Object) => void
+  closeAllModals: () => void
+  closeModal: (modalName?: ModalName) => void
+  closeModals: (modalName: ModalName) => void
 }
 
-export interface Stack<T> {
-  names: Array<ModalName>
-  content: Array<StackItem>
-  defaultOptions: Options
-  openedItems: T
-  total: number
-}
-
-export type EventName = 'onAnimate'
-export type EventNames = Array<EventName>
-
-export type EventCallback = (animatedValue?: AnimatedValue) => void
-
-interface EventSubscription {
-  remove: () => boolean
-}
-
-export type ModalListener = (
-  eventName: EventName,
-  callback: EventCallback,
-) => EventSubscription
-
-export interface EventListeners {
-  event: string,
-  handler: EventCallback
-}
-
-export type ModalEventListeners = Set<EventListeners>
-
-export interface Modal {
+/**
+ * Interface to be used inside a modal component to type
+ * the `modal` prop provided automatically by the library.
+ */
+export interface ModalStackItemProp {
   addListener: ModalListener
-  currentModal: ?ModalName
+  currentModal: ModalName | null
   openModal: (modalName: ModalName, params?: Object) => void
   closeAllModals: () => void
   closeModal: (modalName?: ModalName) => void
@@ -168,30 +206,15 @@ export interface Modal {
   params?: any
 }
 
-export interface WithModal {
-  currentModal: ?ModalName
-  openModal: (modalName: ModalName, params?: Object) => void
-  closeAllModals: () => void
-  closeModal: (modalName?: ModalName) => void
-  closeModals: (modalName: ModalName) => void
+export type ModalEventName = 'onAnimate'
+export type ModalEventCallback = (animatedValue?: AnimatedValue) => void
+export interface ModalEventSubscription {
+  remove: () => boolean
 }
-
-export interface SharedProps {
-  clearListeners: (hash: string) => void
-  closeAllModals: () => void
-  closeModal: (stackItem: StackItem) => void
-  closeModals: (modalName: ModalName) => void
-  currentModal: ?ModalName
-  eventListeners: ModalEventListeners
-  getParams: (hash: ?string, fallback: any) => any
-  openModal: (modalName: ModalName) => void
-  registerListener: (
-    hash: string,
-    eventName: EventName,
-    handler: () => void,
-  ) => EventSubscription
-  stack: Stack<Set<StackItem>>
-}
+export type ModalListener = (
+  eventName: ModalEventName,
+  callback: ModalEventCallback
+) => ModalEventSubscription
 
 
 /**********
@@ -199,14 +222,16 @@ export interface SharedProps {
  *********/
 
 export function createModalStack(
-  config: Config,
-  customDefaultOptions?: Options
-): Stack<Set<StackItem>>
+  config: ModalStackConfig,
+  customDefaultOptions?: ModalStackOptions
+): ModalStack<Set<ModalStackItem>>
 
 export function withModal<P extends object>(
   Component: React.ComponentType<P>
-): React.FC<Omit<P, keyof WithModal>>
+): React.FC<Omit<P, keyof ModalProp>>
 
-export function useModal(): WithModal
+export function useModal(): ModalProp
 
-export class ModalProvider extends React.Component<{ stack: Stack<Set<StackItem>> }> {}
+export class ModalProvider extends React.Component<{
+  stack: ModalStack<Set<ModalStackItem>>
+}> {}
