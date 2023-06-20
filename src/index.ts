1f147c19da12ae66b2ee26308ad1752bef7e1965
Charles Mangwa <charles@colorfy.me>
2023-06-20 13:27:58 +0200

    feat: add`ModalState` to library exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './types'
-export { modalfy } from './lib/ModalState'
 export { default as useModal } from './lib/useModal'
 export { default as withModal } from './lib/withModal'
 export { default as ModalProvider } from './lib/ModalProvider'
+export { default as ModalState, modalfy } from './lib/ModalState'
 export { default as createModalStack } from './lib/createModalStack'
