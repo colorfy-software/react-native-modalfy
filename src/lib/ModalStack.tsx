bf302a872e9f3baed6da7140d81bd70c2774cb18
Egor-Kozlov <kozlov1998.2010@gmail.com>
2025-09-15 10:26:06 +0200

    fix: missing dependency to reset stack options

--- src/lib/ModalStack.tsx
+++ src/lib/ModalStack.tsx
@@ -86,7 +86,7 @@ const ModalStack = <P extends ModalfyParams>(props: Props<P>) => {
     queueMacroTask(() => {
       setModalStackOptions(getStackItemOptions(Array.from(stack.openedItems).pop(), stack))
     })
-  }, [])
+  }, [stack])
 
   const renderStackItem = (stackItem: ModalStackItem<P>, index: number) => {
     const position = stack.openedItems.size - index
